21ffa3e3311b459c1a789817d5856a5e4b614eb6
Josh <joshdibble93@gmail.com>
2019-05-16 20:28:01 +0100

    Adding leaderboard points to realtime DB

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Landing = () => (
-  <div>
-    <h1>Leaderboard</h1>
-    <p>This page is viewable by everyone all the time - use it as the Leaderboard</p>
-  </div>
-);
+import { withAuthorization } from '../Session';
 
-// TO DO 
-// ADD A LEADERBOARD PAGE ACCESSIBLE BY EVERYONE - NOT JUST SIGNED IN USERS
+const INITIAL_STATE = {
+  name: '',
+  points: '',
+}
 
-export default Landing;
+class AddPoints extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { ...INITIAL_STATE }
+  }
+
+  onSubmit = event => {
+    const { name, points } = this.state;
+    console.log(this.state)
+    this.props.firebase
+      .user(points)
+      .set({
+        // name: name,
+        points: points,
+      })
+      .then(authUser => {
+        return this.props.firebase
+          .user(points)
+          .set({
+            username: authUser.user.uid
+          })
+      })
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { name, points } = this.state
+    return (
+      <div>
+        <h1>Host a Run</h1>
+        <form onSubmit={this.onSubmit}>
+          <input value={name} type="text" name="name" onChange={this.onChange} placeholder="Name" />
+          <input value={points} type="text" name="points" onChange={this.onChange} placeholder="Points" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AddPoints);
